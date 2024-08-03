54fd72286ce947f7ded5d5ded5567d6bbf2c0761
rmunozfernandez <rmunoz.it@gmail.com>
2024-08-02 23:36:29 -0400

    Método GET

--- 01-install/main.py
+++ 01-install/main.py
@@ -1,7 +1,41 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
+app.title = "FastAPI - Docs"
+app.version = "0.0.1"
 
-@app.get("/")
+movies = [
+    {
+        "id": 1,
+        "title": "The Shawshank Redemption",
+        "overview": "Two imprisoned",
+        "year": 1994,
+        "rating": 9.3,
+        "category": "Drama"
+    },
+    {
+        "id": 2,
+        "title": "The Godfather",
+        "overview": "The aging patriarch",
+        "year": 1972,
+        "rating": 9.2,
+        "category": "Crime"
+    },
+    {
+        "id": 3,
+        "title": "The Dark Knight",
+        "overview": "When the menace",
+        "year": 2008,
+        "rating": 9.0,
+        "category": "Action"
+    },
+]
+
+@app.get('/', tags=['Home'])
 def message():
-    return "Hello, World!"
+    return HTMLResponse('<h1>Welcome to FastAPI</h1>')
+
+@app.get('/movies', tags=['Movies'])
+def get_movies():
+    return movies
