a568c4462f9141aab51e09fc7815ee95c91a87f5
rmunozfernandez <rmunoz.it@gmail.com>
2024-08-03 18:34:40 -0400

    Método POST

--- 01-install/main.py
+++ 01-install/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from fastapi.responses import HTMLResponse
 
 app = FastAPI()
@@ -50,3 +50,15 @@ def get_movie(id: int):
 @app.get('/movies/', tags=['Movies'])
 def get_movies_by_category(category: str):
     return [movie for movie in movies if movie["category"] == category]
+
+@app.post('/movies', tags=['Movies'])
+def create_movie(id: int = Body(), title: str = Body(), overview: str = Body(), year: int = Body(), rating: float = Body(), category: str = Body()):
+    movies.append({
+        "id": id,
+        "title": title,
+        "overview": overview,
+        "year": year,
+        "rating": rating,
+        "category": category
+    })
+    return movies
